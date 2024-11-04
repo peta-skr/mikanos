0fee5011ac80c001a5c92f3c92278755ebc4c1bf
peta-skr <atu.cho.33@gmail.com>
2024-11-04 07:30:24 +0000

    2024110402

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -4,6 +4,23 @@
 EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
     EFI_SYSTEM_TABLE *system_table) {
+
+  CHAR8 memmap_buf[4096 * 4];
+  struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0,0,0,0};
+  GetMemoryMap(&memmap);
+
+  EFI_FILE_PROTOCOL * rootdir;
+  OpenRootDir(image_handler, &root_dir);
+
+  EFI_FILE_PROTOCOL * memmap_file;
+  root_dir->Open(
+    root_dir, &memmap_file, L"\\memmap",
+    EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE, 0
+  );
+
+  SaveMemoryMap(&memmap, memmap_file);
+  memmap_file->Close(memmap_file);
+
   Print(L"Hello, Mikan World!\n");
   while (1);
   return EFI_SUCCESS;
@@ -39,4 +56,32 @@ EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
     &map->descriptor_size,
     &map->descriptor_version
   );
+}
+EFI_STATUS SaveMemoryMap(struct MemoryMap* map, EIF_FILE_PROTOCOL* file) {
+  CHAR8 buf[256];
+  UINTN len;
+
+  CHAR8* header = "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n";
+  len = AsciiStrLen(header);
+  file->Write(file, &len, header);
+
+  Print(L"map->buffer = %08lx, map->map_size = %08lx\n", map->buffer, map->map_size);
+
+  EFI_PHYSICAL_ADDRESS iter;
+  int i;
+
+  for (iter = (EFI_PHYSICAL_ADDRESS)map->buffer, i = 0;
+    iter < (EFI_PHYSICAL_ADDRESS)map->buffer + map->map_size;
+    iter += map->descriptor_size, i++) {
+      EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*) iter;
+      len = AsciiSPrint(
+        buf, sizeof(buf), "%u, %x, %-ls, %lx, %lx\n",
+        i, desc->Type, GetMemoryTypeUnicode(desc->Type),
+        desc->PhysicalStart, desc->NumberOfPages,
+        desc->Attribute & 0xffffflu
+      );
+      file->Write(file, &len, buf);
+    }
+
+    return EFI_SUCCESS;
 }
